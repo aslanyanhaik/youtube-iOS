d0c0f5c78913d795e3de335a1fbd0959601c64c4
Haik Aslanyan <haik@mexonis.com>
2016-07-04 17:25:50 +0400

    added search Field

--- YouTube/Search.swift
+++ YouTube/Search.swift
@@ -17,8 +17,8 @@ class Search: UIView {
     
     //MARK: Properties
     
-    let searchView: UIView = {
-       let sv = UIView.init(frame: CGRect.init(x: 0, y: 0, width: UIScreen.main().bounds.width, height: 68))
+    lazy var searchView: UIView = {
+       let sv = UIView.init(frame: CGRect.init(x: 0, y: 0, width: self.frame.width, height: 68))
         sv.backgroundColor = UIColor.white()
         sv.alpha = 0
         return sv
@@ -30,6 +30,20 @@ class Search: UIView {
         return bv
     }()
     
+    lazy var backButton: UIButton = {
+       let bb = UIButton.init(frame: CGRect.init(x: 0, y: 20, width: 48, height: 48))
+        bb.setBackgroundImage(UIImage.init(named: "cancel"), for: [])
+        bb.addTarget(self, action: #selector(Search.dismiss), for: .touchUpInside)
+        return bb
+    }()
+    
+    lazy var searchField: UITextField = {
+        let sf = UITextField.init(frame: CGRect.init(x: 48, y: 20, width: self.frame.width - 50, height: 48))
+        sf.placeholder = "Seach on Youtube"
+        sf.keyboardAppearance = .dark
+        return sf
+    }()
+    
     var delegate:hideSearch?
     
     //MARK: Methods
@@ -38,6 +52,7 @@ class Search: UIView {
         UIView.animate(withDuration: 0.3, animations: {
             self.backgroundView.alpha = 0.5
             self.searchView.alpha = 1
+            self.searchField.becomeFirstResponder()
         })
     }
     
@@ -47,6 +62,7 @@ class Search: UIView {
         UIView.animate(withDuration: 0.3, animations: {
             self.backgroundView.alpha = 0
             self.searchView.alpha = 0
+            self.searchField.resignFirstResponder()
             }, completion: {(Bool) in
                 self.delegate?.hideSearchView(status: true)
         })
@@ -58,7 +74,9 @@ class Search: UIView {
         super.init(frame: frame)
         self.addSubview(self.backgroundView)
         self.addSubview(self.searchView)
-    self.backgroundView.addGestureRecognizer(UITapGestureRecognizer.init(target: self, action: #selector(Search.dismiss)))
+        self.searchView.addSubview(self.searchField)
+        self.backgroundView.addGestureRecognizer(UITapGestureRecognizer.init(target: self, action: #selector(Search.dismiss)))
+        self.searchView.addSubview(self.backButton)
     }
 
     required init?(coder aDecoder: NSCoder) {
