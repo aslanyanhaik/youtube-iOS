518639163fadc2541b0497949a045197a5a2d6ee
Haik Aslanyan <haik@mexonis.com>
2016-07-07 16:09:04 +0400

    rearranged the files

---
+++ YouTube/Model/Channel.swift
@@ -0,0 +1,29 @@
+//
+//  Channel.swift
+//  YouTube
+//
+//  Created by Haik Aslanyan on 6/28/16.
+//  Copyright © 2016 Haik Aslanyan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Channel {
+    
+    //MARK Properties
+    
+    let name: String
+    let image: UIImage
+    
+    
+    //MARK: Inits
+    
+    init(name: String, image: UIImage) {
+        self.name = name
+        self.image = image
+        
+    }
+    
+    
+}
