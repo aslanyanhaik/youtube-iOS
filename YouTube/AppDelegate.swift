b12b9b948018725a47c51e63c52df18c68184000
vatsal <vatsalkulshreshtha@gmail.com>
2018-10-29 19:25:59 +0530

    Improved keyboard colour for better UI
    Improved keyboard colour for better UI look. Which also matches with the app theme

--- YouTube/AppDelegate.swift
+++ YouTube/AppDelegate.swift
@@ -28,6 +28,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        UITextField.appearance().keyboardAppearance = .light
         return true
     }
 }
